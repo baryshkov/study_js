f5f34bf11e1e1bb22737036b50e72e2e07c57a83
baryshkov <ambaryshkov@gmail.com>
2019-07-22 18:01:00 +0300

    added new exercise and playground file

---
+++ week3/index.js
@@ -0,0 +1,58 @@
+/**
+ * @param {String} date
+ * @returns {Object}
+ */
+module.exports = function (date) {
+    function convertAndSet(num, format, sign, value) {
+        if (sign === '-') {
+            num *= -1;
+        }
+        // console.info(/^hours$/i.test(format));
+        switch (true) {
+            case /^minutes$/i.test(format):
+                value.setUTCMinutes(value.getUTCMinutes() + num);
+                break;
+            case /^hours$/i.test(format):
+                value.setUTCHours(value.getUTCHours() + num);
+                break;
+            case /^days$/i.test(format):
+                value.setUTCDate(value.getUTCDate() + num);
+                break;
+            case /^months$/i.test(format):
+                value.setUTCMonth(value.getUTCMonth() + num);
+                break;
+            case /^years$/i.test(format):
+                value.setUTCFullYear(value.getUTCFullYear() + num);
+                break;
+        }
+    }
+
+    function check(num, format) {
+        if (!num || !format || num < 0 ||
+            format.search(/^(months|days|hours|minutes|years)$/i) === -1) {
+            throw new TypeError('Incorrect data');
+        }
+    }
+
+    function formatDate(date) {
+        return (date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' '
+            + date.getHours() + ':' + date.getMinutes());
+    }
+
+    var time = new Date(date);
+    return {
+        // time = new Date(date),
+        // tt: console.log(this.time),
+        add: function (num, format) {
+            check(num, format);
+            convertAndSet(num, format, '+', time);
+            return this;
+        },
+        subtract: function (num, format) {
+            check(num, format);
+            convertAndSet(num, format, '-', time);
+            return this;
+        },
+        value: formatDate(time)
+    };
+};
