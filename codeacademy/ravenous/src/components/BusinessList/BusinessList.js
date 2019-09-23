eb4ba48e9a63d52dc3a8f6a3180f157af6eb36a9
baryshkov <ambaryshkov@gmail.com>
2019-09-23 21:59:39 +0300

    updated ravenous logic

--- codeacademy/ravenous/src/components/BusinessList/BusinessList.js
+++ codeacademy/ravenous/src/components/BusinessList/BusinessList.js
@@ -4,4 +4,17 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
 
+  render() {
+    return (
+        <div className="BusinessList">
+          {this.props.businesses.map((business) => {
+           return (
+               <Business business={business} />
+           )
+          })}
+        </div>
+    )
+  }
 }
+
+export default BusinessList;
